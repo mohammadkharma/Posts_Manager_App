223fd4e0ffa43dde0feb47d8cac516d0d1706c83
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-25 17:52:15 +0200

    installed react router

--- posts-manager/src/App.js
+++ posts-manager/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1>Posts Manager</h1>
     </div>
+    </BrowserRouter>
   );
 }
 
