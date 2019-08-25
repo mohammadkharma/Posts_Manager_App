4fcc659ec6ae9a33d200e1c390a2f89f545cd0ee
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-26 00:54:00 +0200

    connecting redux to firestore

--- posts-manager/src/store/actions/postActions.js
+++ posts-manager/src/store/actions/postActions.js
@@ -1,5 +1,5 @@
 export const createPost = post => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async call to database
         dispatch({type: 'CREATE_POST', post })
     }
