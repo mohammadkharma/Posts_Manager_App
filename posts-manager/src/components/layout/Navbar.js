a353b37a924f5b52679ab4d8b94573350054d4c6
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-28 23:55:42 +0200

    styles

--- posts-manager/src/components/layout/Navbar.js
+++ posts-manager/src/components/layout/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = (props) => {
     return(
     <nav className="nav-wrapper grey darken-3">
         <div className="container">
-            <Link to='/' className='brand-logo'>Posts Manager</Link>
+            <Link to='/' className='brand-logo hide-on-med-and-down'>Posts Manager</Link>
             { links }
         </div>
     </nav>       
