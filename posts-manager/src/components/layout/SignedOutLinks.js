028199ff85675b91a065d4f247e1c464b5696820
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-28 21:46:36 +0200

    added welcomeText

--- posts-manager/src/components/layout/SignedOutLinks.js
+++ posts-manager/src/components/layout/SignedOutLinks.js
@@ -5,13 +5,18 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import WelcomeText from './WelcomeText'
+
 
 const SignedOutLinks = () => {
     return(
+    <div className="landing">
     <ul className="right">
         <li><NavLink to='/signup'>Signup</NavLink></li>
         <li><NavLink to='/signin'>Login</NavLink></li>
     </ul>
+    <WelcomeText />
+    </div>
     )
 }
 
---
+++ posts-manager/src/components/layout/WelcomeText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WelcomeText = () => {
+    return (
+        <div className="welcomeText">
+            Welcome to Posts Manager
+        </div>
+    )
+}
+
+export default WelcomeText;
