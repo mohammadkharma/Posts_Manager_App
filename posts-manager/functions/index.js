fb392b97077908dcfe9e4d5325cd9d84d0c9c7be
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-28 16:51:40 +0200

    added a trigger for new user signup notification in cloud functions

--- posts-manager/functions/index.js
+++ posts-manager/functions/index.js
@@ -9,7 +9,7 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from MYK!");
 });
 
-const createNotification = ((notification) => {
+const createNotification = (notification => {
     return admin.firestore().collection('notifications')
       .add(notification)
       .then(doc => console.log('notification added', doc));
@@ -30,3 +30,21 @@ const createNotification = ((notification) => {
       return createNotification(notification);
   
   });
+
+  exports.userJoined = functions.auth.user()
+  .onCreate(user => {
+    
+    return admin.firestore().collection('users')
+      .doc(user.uid).get().then(doc => {
+
+        const newUser = doc.data();
+        const notification = {
+          content: 'Joined the party',
+          user: `${newUser.firstName} ${newUser.lastName}`,
+          time: admin.firestore.FieldValue.serverTimestamp()
+        };
+
+        return createNotification(notification);
+
+      });
+});
