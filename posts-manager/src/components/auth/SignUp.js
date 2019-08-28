77b4967e7ed991455bf8100dae51b506aceb201d
mohammad kharma <mohammad.kh.8688@gmail.com>
2019-08-28 03:36:14 +0200

    handled the sign up

--- posts-manager/src/components/auth/SignUp.js
+++ posts-manager/src/components/auth/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { signUp } from '../../store/actions/authActions/signUpAction';
 
 class SignUp extends Component {
   state = {
@@ -17,9 +18,10 @@ class SignUp extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     // console.log(this.state);
+    this.props.signUp(this.state);
   }
   render() {
-    const { auth } = this.props;
+    const { auth, authError } = this.props;
     if (auth.uid) return <Redirect to='/' />
 
     return (
@@ -39,13 +41,16 @@ class SignUp extends Component {
             <input type="text" id='firstName' onChange={this.handleChange} />
           </div>
           <div className="input-field">
-            <label htmlFor="password">Last Name</label>
-            <input type="text" id='Last Name' onChange={this.handleChange} />
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id='lastName' onChange={this.handleChange} />
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+            <div className="red-text center">
+              {authError ? <p> {authError} </p> : null}
+            </div>
           </div>
-        </form>
+        </form> 
       </div>
     )
   }
@@ -53,8 +58,15 @@ class SignUp extends Component {
 
 const mapState = state => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    authError: state.auth.authError
   }
 }
 
-export default connect(mapState)(SignUp);
+const mapDispatch = dispatch => {
+  return {
+    signUp: newUser => dispatch(signUp(newUser))
+  }
+}
+
+export default connect(mapState, mapDispatch)(SignUp);
--- posts-manager/src/store/reducers/authReducer.js
+++ posts-manager/src/store/reducers/authReducer.js
@@ -15,9 +15,22 @@ const authReducer = (state = initState, action) => {
                 ...state,
                 authError: null
             }
-            case 'SIGNOUT_SUCCESS':
-                console.log('signout success');
-                return state;
+        case 'SIGNOUT_SUCCESS':
+            console.log('signout success');
+            return state;
+        case 'SIGNUP_SUCCESS':
+            console.log('signup success');
+            return {
+                ...state,
+                authError: null
+            }
+        case 'SIGNUP_ERROR':
+            console.log('signup error');
+            return {
+                ...state,
+                authError: action.err.message
+            }
+
         default:
             return state
     }
